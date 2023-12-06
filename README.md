ed6c2914ab7118805d2f2ef8f7b18186ad824276
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-07 02:40:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VirtualSpace
+ DP Group Assessment
---
+++ src/main/java/com/mycompany/VirtualSpace/VirtualSpaceApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.VirtualSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualSpaceApplication.class, args);
+		System.out.println("Application has started!");
+	}
+
+}
