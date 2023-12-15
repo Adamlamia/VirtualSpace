c92a8916ad403f27a4f0fc2eb797992b70d4be9a
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-16 00:14:10 +0800

    Create ImageForm.java
    getter and setter for category and sub category

---
+++ src/main/java/com/mycompany/VirtualSpace/ImageForm.java
@@ -0,0 +1,25 @@
+package com.mycompany.VirtualSpace;
+
+public class ImageForm {
+
+    private String category;
+    private String subcategory;
+
+    // getters and setters
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+}
