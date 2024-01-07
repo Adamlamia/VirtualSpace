61f72e2bbf53804b4eedf0e767527bd26e802998
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2024-01-08 04:32:38 +0800

    Update MyController.java
    removing stuff

--- src/main/java/com/mycompany/VirtualSpace/MyController.java
+++ src/main/java/com/mycompany/VirtualSpace/MyController.java
@@ -1,16 +1,8 @@
 package com.mycompany.VirtualSpace;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/api/virtual-space")
