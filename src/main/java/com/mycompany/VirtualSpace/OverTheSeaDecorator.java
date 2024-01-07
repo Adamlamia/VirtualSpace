2e2d4e4d7ba53ca217fe001b6481887d5e3d2946
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2024-01-08 07:33:26 +0800

    Create OverTheSeaDecorator.java

---
+++ src/main/java/com/mycompany/VirtualSpace/OverTheSeaDecorator.java
@@ -0,0 +1,10 @@
+// OverTheSeaDecorator.java
+package com.mycompany.VirtualSpace;
+
+public class OverTheSeaDecorator implements BackgroundDecorator {
+    @Override
+    public String getImagePath() {
+        // Uncomment the following line and provide the correct image path
+        return "/static/images/over_the_sea.jpg";
+    }
+}
