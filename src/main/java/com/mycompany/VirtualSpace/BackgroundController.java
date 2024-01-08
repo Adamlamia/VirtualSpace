a21891ad9a01823c2cd81ccf08d1e2416e8f71af
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2024-01-08 15:48:31 +0800

    Create template for creating background image decorator

--- src/main/java/com/mycompany/VirtualSpace/BackgroundController.java
+++ src/main/java/com/mycompany/VirtualSpace/BackgroundController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackgroundController {
 
-    private SoundController soundController = new SoundController();
-
     @PostMapping("/change-background")
     public String changeBackground(@RequestParam String option) {
         BackgroundDecorator decorator;
@@ -17,22 +15,17 @@ public class BackgroundController {
         switch (option) {
             case "over-the-sea":
                 decorator = new OverTheSeaDecorator();
-                soundController.addObserver(new ShipBellObserver());
                 break;
             case "in-the-sea":
                 decorator = new InTheSeaDecorator();
-                soundController.addObserver(new SeaWaveObserver());
                 break;
             case "bottom-of-the-sea":
                 decorator = new BottomOfTheSeaDecorator();
-                soundController.addObserver(new SeaBirdObserver());
                 break;
             default:
                 decorator = new DefaultDecorator();
         }
 
-        // Uncomment the following lines and return decorator.getImagePath();
-        soundController.notifyObservers();
         return decorator.getImagePath();
     }
 }
--- src/main/java/com/mycompany/VirtualSpace/BackgroundDecorator.java
+++ src/main/java/com/mycompany/VirtualSpace/BackgroundDecorator.java
@@ -3,3 +3,31 @@ package com.mycompany.VirtualSpace;
 public interface BackgroundDecorator {
     String getImagePath();
 }
+
+class OverTheSeaDecorator implements BackgroundDecorator {
+    @Override
+    public String getImagePath() {
+        return "/images/over_the_sea.jpg";
+    }
+}
+
+class InTheSeaDecorator implements BackgroundDecorator {
+    @Override
+    public String getImagePath() {
+        return "/images/in_the_sea.jpg";
+    }
+}
+
+class BottomOfTheSeaDecorator implements BackgroundDecorator {
+    @Override
+    public String getImagePath() {
+        return "/images/bottom_of_the_sea.jpg";
+    }
+}
+
+class DefaultDecorator implements BackgroundDecorator {
+    @Override
+    public String getImagePath() {
+        return "/images/default.jpg";
+    }
+}
